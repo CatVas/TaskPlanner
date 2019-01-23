68dbff689a74c45ad497af52a63dfc9a396f8d13
Vasyl Samusiev <catvaas@googlemail.com>
2019-01-23 20:07:22 +0200

    Reexports added

--- source/components/Spinner/index.js
+++ source/components/Spinner/index.js
@@ -1,12 +1,2 @@
-// Core
-import React, { Component } from 'react';
 
-// Instruments
-import Styles from './styles.m.css';
-
-export default class Spinner extends Component {
-    render () {
-
-        return <div className = { Styles.spinner } />;
-    }
-}
+export { default } from './Spinner';
---
+++ source/components/index.js
@@ -0,0 +1,4 @@
+
+export { default as Scheduler } from './Scheduler';
+export { default as Spinner } from './Spinner';
+export { default as Task } from './Task';
