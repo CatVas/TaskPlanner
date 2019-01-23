177f279b2026ce425d5cbe5f20bb038be6c061bd
Vasyl Samusiev <catvaas@googlemail.com>
2019-01-23 21:29:44 +0200

    001: Page, Scheduler and AddForm

--- source/components/Scheduler/Scheduler.js
+++ source/components/Scheduler/Scheduler.js
@@ -1,15 +1,31 @@
-// Core
-import React, { Component } from 'react';
 
-// Instruments
+import { AddTask } from 'components';
+import React, { Component } from 'react';
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
 export default class Scheduler extends Component {
+    handleAddTask = (task) => {
+        console.log(task);
+    };
+
     render () {
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <main>
+                    <header>
+                        <h1>
+                            Tasks Planner
+                        </h1>
+                        <input
+                            placeholder = 'Search'
+                            type = 'search'
+                        />
+                    </header>
+                    <section>
+                        <AddTask submit = { this.handleAddTask } />
+                    </section>
+                </main>
             </section>
         );
     }
--- source/components/index.js
+++ source/components/index.js
@@ -1,4 +1,5 @@
 
+export { default as AddTask } from './AddTask';
 export { default as Scheduler } from './Scheduler';
 export { default as Spinner } from './Spinner';
 export { default as Task } from './Task';
